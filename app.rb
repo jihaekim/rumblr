c18111f8902d65e7d73c94b768fc5132303fe00e
Jihae Kim <jihaekim@buffalo.edu>
2018-08-14 16:13:05 -0400

    added view posts

--- app.rb
+++ app.rb
@@ -9,11 +9,17 @@ set :database, {adapter:"postgresql", database:"rumblr"}
 
 get "/" do
 
+
     if session[:user_id]
+        @user = User.find_by(id: session[:user_id])
+        @user_posts= Post.where(user_id: session[:user_id])
         erb :signed_in_homepage
+       
     else
         erb :signed_out_homepage
     end
+
+    
 
 end
 
@@ -43,7 +49,7 @@ end
 # logout
 
 get "/sign-out" do
-    session[:use-id] = nil;
+    session[:user_id] = nil;
     flash[:info] = "You have been signed out"
     redirect '/'
 end
@@ -76,3 +82,49 @@ post "/sign-up" do
 
     redirect '/'
 end
+
+
+get "/create-post" do
+ erb :create_post
+end
+
+
+post "/create-post" do
+@user = User.find_by(id: session[:user_id])
+
+ @post = Post.create(
+     date: params[:date],
+     title: params[:title],
+     photo_url: params[:photourl],
+     content: params[:content],
+      user_id: @user.id
+ )
+#  @user_posts= Post.where(user_id: session[:user_id])
+
+end
+
+
+get'/user/:id' do
+
+ @user_posts= Post.where(user_id: session[:user_id])
+
+ erb :user_posts
+
+end
+
+get'/posts' do
+    @allposts = Post.all
+
+    erb :all_posts
+end
+
+
+def get_current_user 
+    User.find(session[:user_id])
+end
+
+def get_specific_post(id)
+    Post.find(id)
+end
+#also delete posts
+
